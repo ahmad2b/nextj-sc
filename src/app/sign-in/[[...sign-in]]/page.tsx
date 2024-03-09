023baf34aaa98c47682b33573eebe72859e6f82c
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-03-09 13:21:35 +0500

    Refactor CSS and remove unused components

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,9 @@
-import MaxWidthWrapper from '@/components/max-width-wrapper';
 import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
 	return (
-		<MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
+		<div>
 			<SignIn afterSignInUrl={'/quiz'} />
-		</MaxWidthWrapper>
+		</div>
 	);
 }
